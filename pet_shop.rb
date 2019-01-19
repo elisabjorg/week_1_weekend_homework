3e7dca65b5553f92bbf8415bef66404418ecdb44
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-01-19 16:08:17 +0000

    Test five passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,10 +2,22 @@ def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
 
+
 def total_cash(amount)
   return @pet_shop[:admin][:total_cash]
 end
 
+
 def add_or_remove_cash(total_cash, amount)
-  return total_cash[:admin][:total_cash] += amount
+   return @pet_shop[:admin][:total_cash] += amount
+end
+
+
+# def add_or_remove_cash(amount, total_cash)
+#     @petshop[:admin][:total_cash] += amount
+#     @petshop[:admin][:total_cash] -= amount
+# end
+
+def pets_sold(number)
+  return @pet_shop[:admin][:pets_sold]
 end
